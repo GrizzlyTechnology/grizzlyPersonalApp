4b10b2585879b1b80a9d345ec756d98ff782089b
ericma <924402086@qq.com>
2018-08-24 09:29:58 +0800

    解决了全局样式问题

--- src/views/index/registered/app.js
+++ src/views/index/registered/app.js
@@ -1,9 +1,8 @@
 import Vue from 'vue';
 import App from './app.vue';
 import 'muse-ui/lib/styles/base.less';
-// import 'muse-ui/lib/styles/theme.less';
-import 'assets/css/global.less';
 import 'assets/fonts/iconfont.css';
+import 'assets/css/global.less';
 
 if (process.env === 'production') {
   window.apiready = function () {
