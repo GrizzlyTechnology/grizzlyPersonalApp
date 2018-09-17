57605a9a2475ed6e7de3edb9c4e8726e9293d6e2
ericma <924402086@qq.com>
2018-09-17 21:20:22 +0800

    添加了技能表单

--- src/views/userCenter/userSkills/app.vue
+++ src/views/userCenter/userSkills/app.vue
@@ -20,46 +20,97 @@
     <div class="footer">
       <Button color="#009688" textColor="#ffffff" :style="{boxShadow: '0 0 0'}" :full-width="true" large @click="create">添加技能评价</Button>
     </div>
+    <Popup v-model="isShow" position="bottom" pop-transition="popup-fade" :closeOnClickModal="false" class="myPopup">
+      <div class="myPopupTopBar">
+        <span class="cancel" @click="cancel">取消</span>
+        <span class="ok" @click="confirm">保存</span>
+      </div>
+      <Form ref="form" class="form" :model="form">
+        <FormItem label="技能名称"  prop="label" :rules="labelRules">
+          <TextField :readonly="isEdit" v-model="form.label"></TextField>
+        </FormItem>
+        <FormItem label="能力值" prop="major">
+          <span class="level">{{level}}</span>
+          <Slider class="skillLevel" v-model="form.value" :min="1" :step="1" color="#009688" thumb-color="#009688" track-color="#ebebeb" />
+        </FormItem>
+
+      </Form>
+    </Popup>
   </div>
 </template>
 
 <script>
 // import _ from 'lodash';
-import { CellSwipe } from 'mint-ui';
-import { Button } from 'muse-ui';
+import { CellSwipe, Popup } from 'mint-ui';
+import { Button, TextField, Slider } from 'muse-ui';
+import { Form, FormItem } from 'muse-ui/lib/Form';
+
 import tools from 'util/tools';
+import dictMap from 'util/dictMap';
 import service from 'service';
 import SkillLine from 'components/SkillLine';
 // import adapter from 'util/adapter';
 
 // import AreaSelected from 'components/AreaSelected';
+const defaultForm = {
+  label: '',
+  value: 1
+};
 export default {
   data () {
     return {
+      isShow: false,
+      isEdit: false,
       id: window.api ? window.api.pageParam.id : null,
+      form: { ...defaultForm },
+      labelRules: [{ validate: val => !!val, message: '必须填写技能名称' },
+        { validate: val => {
+          let b = true;
+          if (this.isEdit === false) {
+            this.list.forEach(r => {
+              if (r.label === val) {
+                b = false;
+              }
+            });
+          }
+          return b;
+        },
+        message: '技能名称已存在' }],
       // list: window.api ? window.api.pageParam.skills : []
-      list: [{
-        label: 'js',
-        value: 21
-      },
-      {
-        label: 'php',
-        value: 35
-      },
-      {
-        label: 'java',
-        value: 78
-      },
-      {
-        label: 'node',
-        value: 99
-      }]
+      list: [
+        {
+          label: 'js',
+          value: 21
+        },
+        {
+          label: 'php',
+          value: 35
+        },
+        {
+          label: 'java',
+          value: 78
+        },
+        {
+          label: 'node',
+          value: 99
+        }
+      ]
     };
   },
   components: {
     Button,
     CellSwipe,
-    SkillLine
+    SkillLine,
+    Popup,
+    Form,
+    FormItem,
+    TextField,
+    Slider
+  },
+  computed: {
+    level () {
+      return dictMap.skillLevel[parseInt((this.form.value === 0 ? 1 : this.form.value) / 20)];
+    }
   },
   methods: {
     async unLink (skills) {
@@ -76,15 +127,15 @@ export default {
         default:
           tools.toast({
             position: 'top',
-            message: '技能评价删除失败'
+            message: '技能删除失败'
           });
           break;
       }
     },
     del (data, index) {
       tools.confirm({
-        title: '警告',
-        content: `确定要删除 ${data.label}？`,
+        title: '确认',
+        content: `删除 ${data.label} 技能？`,
         callback: () => {
           this.unLink(
             JSON.stringify(
@@ -98,8 +149,23 @@ export default {
         }
       });
     },
-    edit (data, index) {},
-    create () {}
+    edit (data, index) {
+      this.isShow = true;
+      this.isEdit = true;
+      this.form = { ...data };
+    },
+    create () {
+      this.isShow = true;
+      this.isEdit = false;
+      this.form = { ...defaultForm };
+    },
+    cancel () {
+      this.isShow = false;
+      this.$refs.form.clear();
+    },
+    confirm () {
+      this.isShow = false;
+    }
   },
   mounted () {}
 };
@@ -119,8 +185,21 @@ export default {
     display: block;
     width: 100%;
   }
-  .mint-cell-swipe-button{
+  .mint-cell-swipe-button {
     line-height: 56px;
+  }
+}
+.skillLevel {
+  .mu-slider-fill,.mu-slider-track {
+    height: 5px;
+  }
+  .mu-slider-thumb {
+    top: 13px;
+    width: 15px;
+    height: 15px;
+  }
+  .mu-slider-display-value{
+    font-size: 14px;
   }
 }
 </style>
@@ -156,5 +235,39 @@ export default {
 }
 .footer {
   height: 44px;
+}
+
+.myPopup {
+  width: 100%;
+}
+.cancel,
+.ok {
+  font-size: 18px;
+  width: 50%;
+  float: left;
+  text-align: center;
+  padding: 10px 0;
+  box-sizing: border-box;
+  color: #26a2ff;
+  &:active {
+    background-color: #eee;
+  }
+}
+.ok {
+  border-left: 1px #eee solid;
+}
+.myPopupTopBar {
+  font-size: 0;
+  overflow: hidden;
+  border-bottom: 1px #eee solid;
+}
+.form {
+  padding: 15px 15px 0;
+  .level{
+    position: absolute;
+    left: 55px;
+    top: 4px;
+    color: #333;
+  }
 }
 </style>
