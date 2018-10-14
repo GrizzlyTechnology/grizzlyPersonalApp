f1d835769d104dc70d609e741771e3234009ab6b
wanglili <1057252264@qq.com>
2018-10-14 11:10:18 +0800

    新建校外实习页面

---
+++ src/views/practice/outsideSchool/app.vue
@@ -0,0 +1,32 @@
+<template>
+  <Container>
+    <subHeader>
+      目前在职：
+    </subHeader>
+    <Card>
+    <CardMedia title="Image Title" sub-title="Image Sub Title"></cardMedia>
+    </Card>
+  </Container>
+</template>
+
+<script>
+import { Container } from 'muse-ui/lib/Grid';
+import { Card, cardMedia } from 'muse-ui';
+// import { Form, FormItem } from 'muse-ui/lib/Form';
+
+export default {
+  data () {
+    return {};
+  },
+  components: {
+    Container,
+    Card,
+    cardMedia
+  },
+  methods: {},
+  mounted () {}
+};
+</script>
+<style lang="less" scoped>
+@import url("../../../assets/css/base.less");
+</style>
