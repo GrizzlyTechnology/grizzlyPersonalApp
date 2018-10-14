2797d753d26c2437f82016f802386b3b4d0afd39
wanglili <1057252264@qq.com>
2018-10-14 15:14:12 +0800

    校外实习页面，实习协议列表编写

---
+++ src/views/application/internshipAgreement/app.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <List>
+      <ListItem>
+        <ListItemTitle>
+          江苏大灰熊科技有限公司
+        </ListItemTitle>
+        <ListItemAction>
+          程序组
+        </ListItemAction>
+        <ListItemAction>
+          技术部
+        </ListItemAction>
+      </ListItem>
+    </List>
+  </div>
+</template>
+
+<script>
+import {
+  List,
+  ListItem,
+  ListItemAction,
+  ListItemTitle
+} from 'muse-ui/lib/List';
+// import tool from 'util/tools';
+export default {
+  data () {
+    return {};
+  },
+  components: {
+    List,
+    ListItem,
+    ListItemAction,
+    ListItemTitle
+  },
+  methods: {},
+  mounted () {}
+};
+</script>
+<style lang="less" scoped>
+@import url("../../../assets/css/base.less");
+</style>
