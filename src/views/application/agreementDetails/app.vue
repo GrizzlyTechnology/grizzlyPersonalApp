c9ec7b0871863f78cd8f7ff7c1255041e1a0e859
ericma <924402086@qq.com>
2018-11-23 20:46:09 +0800

    协议详情

--- src/views/application/agreementDetails/app.vue
+++ src/views/application/agreementDetails/app.vue
@@ -11,24 +11,27 @@
     <div class="picList">
       <div class="picCon" v-for="(file,index) in picList" :key="file.url">
         <div class="con" @click="imagesPopupOpen(picList,index,'')" :style="{backgroundImage:'url('+file.coverUrl+')'}" />
+        <Icon class="close" :size="18" value=":el-icon-error" color="#000" @click="remove(file,index)"/>
       </div>
       <div class="infoNotice" v-if="picList.length===0">
         暂无协议上传
       </div>
     </div>
-    <div class="footer">
-      <Button color="#009688" textColor="#ffffff" :full-width="true" :style="{boxShadow: '0 0 0'}" large @click="doChoice(statusMap.deliveryLeaveSchool)">立即上传</Button>
-    </div>
+    <Upload class="footer" ref="uploader" accept="image/*" name="file" :show-file-list="false" :with-credentials="true" :multiple="false" :data="{type:2}" :headers="headers" :action="actionUrl" :file-list="uploaderLiist" :on-change="change" :on-progress="progress" :on-success="success" :before-upload="beforeUpload" :on-error="error">
+      <Button color="#009688" textColor="#ffffff" :full-width="true" :style="{boxShadow: '0 0 0'}" large>立即上传</Button>
+    </Upload>
     <ImagesPopup ref="imagesPopup" :urlList="urlList" :index="urlListIndex" :description="description"></ImagesPopup>
   </div>
 </template>
 
 <script>
-import { Button } from 'muse-ui';
+import { Button, Icon } from 'muse-ui';
 import ImagesPopup from 'components/ImagesPopup';
-// import Upload from 'element-ui/lib/upload';
+import Upload from 'element-ui/lib/upload';
 // import Progress from 'element-ui/lib/progress';
 import tools from 'util/tools';
+import { hostList } from 'service/mock';
+
 export default {
   data () {
     return {
@@ -54,12 +57,32 @@ export default {
           coverUrl:
             'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1634998881,2572259975&fm=26&gp=0.jpg'
         }
-      ]
+      ],
+
+      uploaderHide: false,
+      uploaderLiist: [],
+      actionUrl:
+        'http://' +
+        (process.env === 'production' ? hostList.pro : hostList.test) +
+        '/api/Userresources/create',
+      // actionUrl: 'https://jsonplaceholder.typicode.com/posts/',
+      headers: {
+        MG_code:
+          '5uwPulFblsIANI7BIP#a%bBo582#wOud3v%f0c1JgJRskqUTN7y4&TPUTgjkmhOjZI#oVc4Ph4Ar^ApQFy$ZlGl3T9MaIskgGWTVjqHxsP^8S^%gY#nAj9X4DV9x&b7O',
+        MG_key: '5b10fed636fcf',
+        MG_token:
+          process.env === 'development'
+            ? '6f8bade35ef87e5a6aa623519ef973582dc25205'
+            : tools.getStorage('token') || ''
+      },
+      maxSize: 10
     };
   },
   components: {
     Button,
-    ImagesPopup
+    Upload,
+    ImagesPopup,
+    Icon
   },
   methods: {
     companyDetail (id) {
@@ -83,6 +106,47 @@ export default {
       });
       this.urlListIndex = index;
       this.$refs.imagesPopup.open();
+    },
+    beforeUpload (file) {
+      if (file.size > this.maxSize * 1024 * 1024) {
+        tools.toast({
+          position: 'top',
+          message: '图片最大' + this.maxSize + 'M'
+        });
+        return false;
+      }
+    },
+    progress (file) {
+      tools.showProgress();
+    },
+    error (e, file, fileList) {
+      tools.hideProgress();
+      tools.toast({
+        position: 'top',
+        message: '协议上传出错，请稍后重试'
+      });
+    },
+    success (response, file, fileList) {
+      tools.hideProgress();
+      switch (response.code) {
+        case 0:
+          const urlAry = response.result.file.url.split('/');
+          urlAry[urlAry.length - 1] = '450_' + urlAry[urlAry.length - 1];
+          this.picList.push({
+            url: response.result.file.url,
+            coverUrl: urlAry.join('/')
+          });
+          break;
+        default:
+          tools.toast({
+            position: 'top',
+            message: response.message
+          });
+          break;
+      }
+    },
+    remove (file, index) {
+      this.picList.splice(index, 1);
     },
     submit () {
       this.$refs.form.validate().then(result => {
@@ -158,14 +222,20 @@ export default {
 .picList {
   flex: 1;
   overflow: auto;
-  margin-bottom: 15px;
   font-size: 0;
-  margin: -5px;
+  margin: -5px -5px 15px;
   .picCon {
     width: 50%;
     padding-top: 50%;
     display: inline-block;
     position: relative;
+    .close{
+      position: absolute;
+      right: 10px;
+      top: 10px;
+      background-color: #fff;
+      border-radius: 9px;
+    }
     .con {
       position: absolute;
       left: 5px;
@@ -201,5 +271,10 @@ export default {
   padding: 20px 15px;
   text-align: center;
   color: #a2d4f7;
+}
+.footer {
+  .el-upload {
+    width: 100%;
+  }
 }
 </style>
